fbac820e16410cfae940ea9dbfd65214c62da17d
schnozerman <164568439+schnozerman@users.noreply.github.com>
2024-04-02 17:58:10 -0400

    Update arduinoserial_with_website.py
    fixed the port start

--- arduinoserial_with_website.py
+++ arduinoserial_with_website.py
@@ -5,8 +5,9 @@ import pygetwindow as gw
 from pynput.keyboard import Key, Controller
 
 '''defining functions'''
+arduino = serial.Serial('com3', 9600)
 def main():
-    arduino = serial.Serial('com3', 9600)
+
     data = arduino.readline()
     decoded = str(data[0:len(data)].decode('utf-8'))
     return decoded.strip()
@@ -16,7 +17,7 @@ def main():
 '''first open the website and let it act as the main hub ish'''
 os.system("start \"\" http://dawtrek.crcrobotics.com/")
 
-time.sleep(1) # gives time for file to actually start
+time.sleep(2.5) # gives time for file to actually start
 print(gw.getAllTitles(), end = '\n') # Debug to see if im fucking up
 window1 = gw.getWindowsWithTitle('DAW TREK - Google Chrome')[0]
 window1.maximize()
@@ -31,29 +32,28 @@ videoplaying = False
 while True:
     inp = main()
     print(inp)
+
     '''input from the arduino goes from 0 to 3 which corresponds to a video or no video'''
     if (inp == '1') and videoplaying == False:
-        os.startfile('C:\\Users\\William\\Videos\\Captures\\(6) Gordon Ramsay Smashes Salmon _ comp _ - YouTube - Google Chrome 2023-01-11 01-56-28.mp4')
-        time.sleep(1)  # gives time for file to actually start
-        window = gw.getWindowsWithTitle('VLC media player')[0]
+        os.startfile("C:\\Users\\tinhr\\OneDrive\\Images\\Projets vidéo\\ALERTE_ROUGE.mp4")
+        time.sleep(2)
+        window = gw.getWindowsWithTitle('Lecteur multimédia VLC')[0]
         window.maximize()
         videoplaying = True
 
     elif inp == '2' and videoplaying == False:
-        os.startfile('C:\\Users\\William\\Videos\\Captures\\Dawson CRC Robotics Tutorial for Kryptik 2024_ How to pick a CRC Theme.mp4')
-        time.sleep(1)  # gives time for file to actually start
-        window = gw.getWindowsWithTitle('VLC media player')[0]
+        os.startfile("C:\\Users\\tinhr\\OneDrive\\Images\\Projets vidéo\\QVEY8403.MOV")
+        time.sleep(2)  # gives time for file to actually start
+        window = gw.getWindowsWithTitle('Lecteur multimédia VLC')[0]
         window.maximize()
-        videoplaying = True
 
-    elif inp == '2' and videoplaying == False:
-        os.startfile('C:\\Users\\William\\Videos\\Captures\\Dawson CRC Robotics Tutorial for Kryptik 2024_ How to pick a CRC Theme.mp4')
-        time.sleep(1)  # gives time for file to actually start
-        window = gw.getWindowsWithTitle('VLC media player')[0]
-        window.maximize()
+
         videoplaying = True
 
     elif inp == '0' and videoplaying == True:
+        print(gw.getAllTitles())
+        window = gw.getWindowsWithTitle('Lecteur multimédia VLC')[0]
         window.close() # when turning off switch and theres a video playing, we close the video window to go back to the mainhub, the website
         videoplaying = False
+
 
